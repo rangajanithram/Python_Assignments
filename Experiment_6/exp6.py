e0f411bde38d9844428cdcade84005991199cf46
Ranga Janith Ram <rangajanithram2006@gmail.com>
2026-03-11 22:13:40 +0530

    Refactor print statements in dictionaries function

--- Experiment_6/exp6.py
+++ Experiment_6/exp6.py
@@ -1,36 +1,35 @@
 def dictionaries():
-   
-    
     student = {
         "name": "Alice",
         "age": 22,
         "major": "Computer Science",
         "gpa": 3.8
     }
-    print(f"Original Dictionary: {student}")
     
-    print(f"Student's Name: {student['name']}")
+    print("Original Dictionary:", student)
     
-    print(f"Student's GPA using get: {student.get('gpa')}")
+    print("Student's Name:", student['name'])
+    
+    print("Student's GPA using get:", student.get('gpa'))
     
     student["graduation year"] = 2026
     student["gpa"] = 3.9
-    print(f"After updating: {student}")
+    print("After updating:", student)
     
     student.update({"age": 23, "honors": True})
-    print(f"After update : {student}")
+    print("After update :", student)
     
-    print(f"Dictionary Keys: {student.keys()}")
-    print(f"Dictionary Values: {student.values()}")
-    print(f"Dictionary Items: {student.items()}")
+    print("Dictionary Keys:", student.keys())
+    print("Dictionary Values:", student.values())
+    print("Dictionary Items:", student.items())
     
     removed_major = student.pop("major")
-    print(f"Removed Major: {removed_major}")
+    print("Removed Major:", removed_major)
     
     last_item = student.popitem()
-    print(f"Removed last item: {last_item}")
+    print("Removed last item:", last_item)
     
     student.clear()
-    print(f"Dictionary after clear(): {student}")
+    print("Dictionary after clear():", student)
 
 dictionaries()
